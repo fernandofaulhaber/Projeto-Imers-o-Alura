e63c38341a7ce05958d84184d18a02385b316277
fernandofaulhaber <139382029+fernandofaulhaber@users.noreply.github.com>
2024-09-06 15:36:32 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : "Aspas",
+        descricao : "Aspas, cujo nome verdadeiro é Erick Santos, é um jogador profissional brasileiro de VALORANT que rapidamente se tornou um dos nomes mais reconhecidos da cena competitiva mundial.",
+        link: "https://liquipedia.net/valorant/Aspas",
+        tags: "melhor do mundo leviatan Leviatan Lev campeão campeao"
+     },
+    
+     {
+        titulo : "Sacy",
+        descricao: "Sacy, cujo nome verdadeiro é Gustavo Rossi, é uma figura fundamental no cenário competitivo de VALORANT, especialmente no Brasil. Conhecido por sua visão de jogo estratégica e liderança inigualável",
+        link: "https://liquipedia.net/valorant/Sacy",
+        tags: "careca loud sentinels"
+     },
+     {
+        titulo : "TenZ",
+        descricao: "Tyson TenZ Ngo é um dos jogadores de VALORANT mais famosos e talentosos do mundo. Conhecido por suas habilidades impecáveis, especialmente com o duelista Jett.",
+        link: "https://liquipedia.net/valorant/TenZ",
+        tags: "sentinels c9 promessa"
+     }
+     ];
+
+    
